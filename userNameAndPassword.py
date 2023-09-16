91330c0c207ffe7a121b6172f2ac70d94d5bb78f
daley-flips <drf58@pitt.edu>
2023-09-16 11:36:21 -0400

    some file
    creating an account :)

---
+++ userNameAndPassword.py
@@ -0,0 +1,11 @@
+class userNameAndPassword:
+    def createUsername():
+        userName = input("Username: ")
+        print('Username Set to ', userName)
+
+    def createPassword():
+        password = input("Password: ")
+        print('Password Set to ', password)
+
+    createUsername()
+    createPassword()
